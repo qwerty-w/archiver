8f343541c56bc28e63b4e66d8ce476904089dd53
qwerty <itsqwz@gmail.com>
2023-06-11 23:33:42 +0300

    Add onepass method support, large compression.py refactoring

--- huffman/compression.py
+++ huffman/compression.py
@@ -1,7 +1,7 @@
-import time
 import typing
 from functools import partial
-from huffman import BitArray, BitStream, ConstBitStream
+from abc import ABC
+from bitstring import BitStream, ConstBitStream
 
 
 class InnerNode:  # PlaceHolder
@@ -9,12 +9,19 @@ class InnerNode:  # PlaceHolder
         return '<InnerNode>'
 
 
+class NYT:
+    pass
+
+
 class Node:
-    def __init__(self, value: bytes | InnerNode, freq: int, *, left: 'Node' = None, right: 'Node' = None):
-        self.left = left  # 0
-        self.right = right  # 1
+    def __init__(self, value: bytes | type[InnerNode, NYT], freq: int, *,
+                 parent: 'Node' = None, left: 'Node' = None, right: 'Node' = None):
+
         self.value = value
         self.freq = freq
+        self.parent = parent
+        self.left = left  # 0
+        self.right = right  # 1
 
     def __str__(self):
         return str(self.value)
@@ -44,40 +51,36 @@ class Wrapper:
 # noinspection PyArgumentList
 class Encoding:
     """
-    <>
+    <FULL encoding size:2> <max bits length:1> <0111 1001001> <...>
+                                                size  value
     """
-    def __init__(self, table: dict[bytes, BitArray], max_code_length: int = None):
+    def __init__(self, table: dict[bytes, str], max_code_length: int = None):
         self.table = table
         self.max_code_length = max_code_length if max_code_length is not None else self.get_max_code_length()
 
     def get_max_code_length(self) -> int:
-        return sum(map(len, self.table.values()))
+        return max(map(len, self.table.values()))
 
-    def get_reversed_table(self) -> dict[BitArray, bytes]:
+    def get_reversed_table(self) -> dict[str, bytes]:
         return dict(zip(self.table.values(), self.table.keys()))
 
-    # <FULL encoding size:2> <max bits length:1(7 for example)> <0111 1001001>
-    #                                                            size  value
     def pack(self) -> bytes:
         code_length_size = (self.max_code_length - 1).bit_length()
 
         if code_length_size >= 256:
             raise ValueError('max code length is too big')
 
-        barray = BitArray()
+        auto = ''
         for byte, bits in self.table.items():
             code_length = len(bits) - 1
 
             if code_length < 0:
                 raise ValueError(f'code for byte {byte} is empty')
 
-            for val in [
-                '0x' + byte.hex(),
-                '0b' + '0' * (code_length_size - code_length.bit_length()) + bin(code_length)[2:],
-                bits
-            ]: barray.append(val)
+            auto += bin(byte[0])[2:].zfill(8) + bin(code_length)[2:].zfill(code_length_size) + bits
 
-        rv = barray.tobytes()
+        auto = auto + '0' * (8 - len(auto) % 8 if len(auto) % 8 else 0)
+        rv = b''.join(int(auto[i:i + 8], 2).to_bytes(1) for i in range(0, len(auto), 8))
 
         if len(rv) >= 256 ** 2:
             raise ValueError('encoding is too big')
@@ -107,7 +110,7 @@ class Encoding:
             fill(code_length_size)
             code_length = data.read(f'uint{code_length_size}') + 1
             fill(code_length)
-            table[byte] = BitArray(data.read(f'bits{code_length}'))
+            table[byte] = data.read(f'bits{code_length}').bin
 
         return cls(table)
 
@@ -115,8 +118,8 @@ class Encoding:
         return self.get_reversed_table()
 
 
-class Encoder:  # HuffmanCoding
-    def __init__(self, stream: ConstBitStream):
+class TwoPassEncoder:  # HuffmanCoding
+    def __init__(self, stream: typing.BinaryIO):
         self.stream = stream
         self.root = self.build_tree()
         self.encoding, self.encoded_size, self.encoded_offset = self._get_encs()
@@ -139,22 +142,19 @@ class Encoder:  # HuffmanCoding
         return nodes.pop(i1), nodes.pop(i2 - 1 if i2 > i1 else i2)
 
     def build_tree(self) -> Node:
-        freq = {}
-
-        while self.stream.pos < self.stream.length:
-            byte = self.stream.read('bytes1')
-
+        freq, pos = {}, self.stream.tell()
+        while byte := self.stream.read(1):
             if byte in freq:
                 freq[byte].freq += 1
 
             else:
                 freq[byte] = Node(byte, 1)
-        self.stream.pos = 0
+        self.stream.seek(pos)
 
         nodes = list(freq.values())
         while len(nodes) > 1:
             left, right = self._pop2smallest(nodes)
-            nodes.append(Node(InnerNode(), left.freq + right.freq, left=left, right=right))
+            nodes.append(Node(InnerNode, left.freq + right.freq, left=left, right=right))
 
         root = nodes.pop()
         return root
@@ -162,8 +162,7 @@ class Encoder:  # HuffmanCoding
     def _get_encs(self) -> tuple[Encoding, int, int]:
         encoding = {}
 
-        stack = [Wrapper(self.root)]
-        code = BitArray()
+        stack, code = [Wrapper(self.root)], ''
         max_code_length = bit_size = 0
         while stack:
             wrapper = stack[-1]
@@ -172,17 +171,17 @@ class Encoder:  # HuffmanCoding
             if current.left and wrapper.next_left():
                 wrapper.state = wrapper.RIGHT
                 stack.append(Wrapper(current.left))
-                code += '0b0'
+                code += '0'
 
             elif current.right and wrapper.next_right():
                 wrapper.state = None
                 stack.append(Wrapper(current.right))
-                code += '0b1'
+                code += '1'
 
             else:
                 n = stack.pop().node
 
-                if not isinstance(n.value, InnerNode):
+                if n.value is not InnerNode:
                     encoding[n.value] = code
                     bit_size += len(code) * n.freq
                     if len(code) > max_code_length:
@@ -195,21 +194,22 @@ class Encoder:  # HuffmanCoding
                 bit_size // 8 + (remainder > 0),
                 8 - remainder if remainder else 0)
 
+    # noinspection PyArgumentList
     def encode(self) -> typing.Generator[bytes, None, None]:
-        yv = BitArray()
+        yv = ''
 
-        while self.stream.pos < self.stream.length:
-            if yv.length >= 8:
-                b, yv = yv[:8].tobytes(), yv[8:]
+        while byte := self.stream.read(1):
+            if len(yv) >= 8:
+                b, yv = int(yv[:8], 2).to_bytes(1), yv[8:]
                 yield b
 
-            byte = self.stream.read('bytes1')
             yv += self.encoding.table[byte]
 
-        yield yv.tobytes()
+        yv += '0' * (8 - len(yv) % 8 if len(yv) % 8 else 0)
+        yield int(yv, 2).to_bytes(len(yv) // 8)
 
 
-class Decoder:
+class TwoPassDecoder:
     def __init__(self, encoding: Encoding, stream: ConstBitStream, segment_size: int = 64):
         self.encoding = encoding
         self.stream = stream
@@ -220,69 +220,150 @@ class Decoder:
         return next(self)
 
     def __next__(self) -> typing.Generator[bytes, None, None]:
-        code = BitArray()
+        code, write_segment = '', b''
+        for bit in self.stream:
+            code += '1' if bit else '0'
+            if code in self.table:
+                write_segment += self.table[code]
+                code = ''
 
-        for segment in self.stream.cut(self.segment_size):
-            for bit in segment.bin:
-                code += '0b' + bit
+                if len(write_segment) >= self.segment_size:
+                    yield write_segment
+                    write_segment = b''
 
-                if code in self.table:
-                    yield self.table[code]
-                    code.clear()
+        yield write_segment
 
     def decode(self):
         return iter(self)
 
 
-def time_wrap(func, *args, _round: int = 2):
-    t0 = time.time()
-    rv = func(*args)
-    return int((time.time() - t0) * 10 ** _round) / 10 ** _round, rv
+class OnePassMethod(ABC):
+    def __init__(self):
+        self.root = self.nyt = Node(NYT, 0)
+        self.nodes = []
+        self.seen: list[Node | None] = [None] * 256
+
+        self.segment_size = 64  # bytes
+
+    def find_largest_node(self, freq: int) -> Node:
+        for n in reversed(self.nodes):
+            if n.freq == freq:
+                return n
+
+    def swap_node(self, n1: Node, n2: Node):
+        i1, i2 = self.nodes.index(n1), self.nodes.index(n2)
+        self.nodes[i1], self.nodes[i2] = self.nodes[i2], self.nodes[i1]
+        n1.parent, n2.parent = n2.parent, n1.parent
+
+        # replace parent's child
+        for _n, _is, _rp in [
+            (n1, n2, n1), (n2, n1, n2)
+        ]:
+            setattr(_n.parent, 'left' if _n.parent.left is _is else 'right', _rp)
+
+    def insert(self, value: bytes):
+        node = self.seen[value[0]]
+
+        if node is None:
+            n = Node(value, 1)
+            inner = Node(InnerNode, 1, parent=self.nyt.parent, left=self.nyt, right=n)
+            n.parent = self.nyt.parent = inner
+
+            if inner.parent:
+                inner.parent.left = inner
+
+            else:
+                self.root = inner
+
+            for _n in [inner, n]:
+                self.nodes.insert(0, _n)
+
+            self.seen[value[0]] = n
+            node = inner.parent
+
+        while node is not None:
+            largest = self.find_largest_node(node.freq)
+
+            if node not in [largest, largest.parent] and largest is not node.parent:
+                self.swap_node(node, largest)
+
+            node.freq = node.freq + 1
+            node = node.parent
 
 
-def main():
-    # enc = Encoding({
-    #     b'\xf0': BitArray('0b10101011'),
-    #     b'\xee': BitArray('0b1010'),
-    #     b'\x24': BitArray('0b101'),
-    #     b'\x00': BitArray('0b101010'),
-    #     b'\xff': BitArray('0b001')
-    # })
-    # e = enc.pack()
-    # BitArray(e)[4 * 8:].pp()
-    # from io import BytesIO
-    # d = enc.unpack(BytesIO(e))
-    # print(d.table)
-    # return
+class OnePassEncoder(OnePassMethod):
+    def __init__(self, stream: typing.BinaryIO):
+        super().__init__()
+        self.stream = stream
+        self._size = self._offset = None
 
-    string = b'Mama mila ramu'
-    print(f'String [{len(string)}]:', string, end='\n\n')
+    def _dfs_find(self, v: bytes | type[NYT], node: Node, code: str = '') -> str:
+        if node.left is None and node.right is None:
+            return code if node.value == v else ''
 
-    e = Encoder(ConstBitStream(string))
+        _code = ''
+        if node.left:
+            _code = self._dfs_find(v, node.left, code + '0')
+        if not _code and node.right:
+            _code = self._dfs_find(v, node.right, code + '1')
 
-    encoded = b''
-    for i in e.encode():
-        encoded += i
+        return _code
 
-    # print(encoded)
+    def get_code(self, v: bytes | type[NYT]):
+        return self._dfs_find(v, self.root, '')
 
-    from io import BytesIO
-    stream: ConstBitStream = ConstBitStream(BytesIO(encoded), length=e.encoded_size * 8 - e.encoded_offset)
-    d = Decoder(e.encoding, stream)
+    def get_size_inf(self) -> tuple[int, int]:
+        if self._size is None or self._offset is None:
+            raise RuntimeError('size and offset can only be obtained after end of .encode()')
 
-    decoded = b''
-    for i in d.decode():
-        decoded += i
+        return self._size, self._offset
 
-    print(decoded)
+    # noinspection PyArgumentList
+    def encode(self) -> typing.Generator[bytes, None, None]:
+        code = ''
+        size = 0
 
-    # t_e, encoded = time_wrap(h.encode)
-    # t_d, decoded = time_wrap(HuffmanCoding.decode, h.encoding, encoded)
-    #
-    # print('Encoding:', dict(map(lambda x: (chr(x[0]), x[1]), h.encoding.items())))
-    # print(f'Encoded string [{t_e}/{len(encoded)} bytes]:', bin(int.from_bytes(encoded, 'big', signed=False)))
-    # print(f'Decoded string [{t_d}/{len(string)} bytes]:', decoded)
+        while data := self.stream.read(self.segment_size):
+            for integer in data:
+                byte = integer.to_bytes(1)
+
+                code += self.get_code(byte) if self.seen[integer] \
+                    else self.get_code(NYT) + bin(integer)[2:].zfill(8)
+                self.insert(byte)
+
+                if len(code) > 8:
+                    yv, code = code[:8], code[8:]
+                    size += 1
+                    yield int(yv, 2).to_bytes(1)
+
+        self._offset = 8 - len(code) % 8 if len(code) % 8 else 0
+        code += '0' * self._offset
+        yv = int(code, 2).to_bytes(len(code) // 8)
+        self._size = size + len(yv)
+        yield yv
 
 
-if __name__ == '__main__':
-    main()
+class OnePassDecoder(OnePassMethod):
+    def __init__(self, stream: ConstBitStream):
+        super().__init__()
+        self.stream = stream
+
+    def decode(self) -> typing.Generator[bytes, None, None]:
+        # noinspection PyRedundantParentheses
+        yield (fb := self.stream.read('bytes1'))
+        self.insert(fb)
+        node = self.root
+
+        while self.stream.pos < self.stream.length:
+            node = node.right if self.stream.read('bool') else node.left
+            value = node.value
+
+            if value is InnerNode:
+                continue
+
+            if value is NYT:
+                value = self.stream.read('bytes1')
+
+            yield value
+            self.insert(value)
+            node = self.root
