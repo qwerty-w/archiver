45622d9874c07951e047241287f5180c1f9fbb86
qwerty <euotao@gmail.com>
2023-06-12 00:18:14 +0300

    Update test_archive.py for archive.py changes

--- tests/test_archive.py
+++ tests/test_archive.py
@@ -1,12 +1,12 @@
 import random
 import pytest
 from io import BytesIO
-from huffman import zip
+from huffman import archive, compression
 
 
 @pytest.fixture(params=range(1, 8))
 def varint(request):
-    return zip.VarInt(request.param)
+    return archive.VarInt(request.param)
 
 
 @pytest.fixture(params=[lambda vi: vi.max_value, lambda vi: 256 ** vi.size])
@@ -22,29 +22,28 @@ def test_varint(rand_varint):
 
 @pytest.fixture
 def h(request):
-    return zip.Header(*TestHeader.test_args)
+    return archive.Header(*TestHeader.test_args)
 
 
 class TestHeader:
     test_args = [
-        'filename', 5729813, 279813, 4, {
-            b'\xf0': zip.BitArray('0b101010'),
-            b'\xee': zip.BitArray('0b1010'),
-            b'\x24': zip.BitArray('0b101'),
-            b'\x00': zip.BitArray('0b1010101'),
-            b'\xff': zip.BitArray('0b001')
-        }
+        'filename', 5729813, 279813, 4, 'twopass', compression.Encoding({
+            b'\xf0': '101010',
+            b'\xee': '1010',
+            b'\x24': '101',
+            b'\x00': '1010101',
+            b'\xff': '001'
+        })
     ]
-    test_serialized_header = b'\x08filename\xfc\x57\x6e\x15\xfc\x04\x45\x05\x04\xf0\x01\x00\x01\x00\x01\x00' \
-                             b'\n\xee\x01\x00\x01\x00\n\x24\x01\x00\x01\n\x00\x01\x00\x01\x00\x01\x00\x01\n' \
-                             b'\xff\x00\x00\x01\n\n'
+    test_serialized_header = b'\x08filename\xfc\x57\x6e\x15\xfc\x04\x45\x05\x04\x01\x00\x0a\x03\xf0\xb5\x77' \
+                             b'\x3a\x24\x54\x03\x55\xff\x44'
 
-    def test_offset_pos(self, h):
-        assert h.get_offset_position() == 17
+    def test_compressed_size_pos(self, h):
+        assert h.get_compressed_size_pos() == 13
 
     def test_encoding_pack_unpack(self, h):
-        assert h.pack_encoding() == self.test_serialized_header[18:]
-        assert h.unpack_encoding(BytesIO(self.test_serialized_header[18:])) == self.test_args[-1]
+        assert self.test_serialized_header[19:] == h.encoding.pack()
+        assert compression.Encoding.unpack(BytesIO(self.test_serialized_header[19:])).table == self.test_args[-1].table
 
     def test_pack_unpack(self, h):
         assert h.pack() == self.test_serialized_header
