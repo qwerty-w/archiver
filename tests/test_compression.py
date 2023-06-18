bafed94f06893ea50ef96e9222ac6900f1d312e3
qwerty <itsqwz@gmail.com>
2023-06-18 15:16:47 +0300

    Add tests for compression.Encoding

--- tests/test_compression.py
+++ tests/test_compression.py
@@ -1,3 +1,4 @@
+import io
 import os
 import hashlib
 from dataclasses import dataclass
@@ -14,6 +15,37 @@ class File:
 
 
 class TestCompression:
+    TABLE = {
+        b'\xff': '101010',
+        b'\xef': '1010',
+        b'\x12': '101',
+        b'\x00': '1010101',
+        b'\xf0': '001',
+        b'\x04': '100'
+    }
+    REVERSED_TABLE = {
+        '101010': b'\xff',
+        '1010': b'\xef',
+        '101': b'\x12',
+        '1010101': b'\x00',
+        '001': b'\xf0',
+        '100': b'\x04'
+    }
+    MAX_CODE_LENGTH = 7
+    PACKED_ENCODING = b'\x00\x0c\x03\xff\xb5\x77\xba\x12\x54\x03\x55\xf0\x44\x11\x40'
+
+    def test_encoding(self):
+        enc = compression.Encoding(self.TABLE)
+        assert enc.max_code_length == self.MAX_CODE_LENGTH == \
+               compression.Encoding(self.TABLE, self.MAX_CODE_LENGTH).max_code_length
+        assert enc.pack() == self.PACKED_ENCODING
+        assert enc.get_reversed_table() == self.REVERSED_TABLE == reversed(enc)
+
+        unpacked = compression.Encoding.unpack(io.BytesIO(self.PACKED_ENCODING))
+        assert unpacked.table == self.TABLE
+        assert unpacked.max_code_length == self.MAX_CODE_LENGTH
+        assert unpacked == enc  # __eq__ test
+
     FILES = [File('data/Tyger',
                   'D2870D04898C66CD04EFA28E3D8A716889B9024791DE76F47819835140479D94',
                   {'onepass': '6E7930E2933E0721C171CE2FFEA1A02B749D3C4037565D282B9A38CA372F6B3B',
